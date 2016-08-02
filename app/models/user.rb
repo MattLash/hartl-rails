b926ae1718a0a8b33643437847f55e1b9d0874a7
Matt Lashinsky <malashinsky@gmail.com>
2016-08-02 21:42:13 +0000

    up to 9.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
     before_save  {self.email = email.downcase }
     has_many :microposts
     validates :name, presence: true, length: {maximum: 50}
@@ -6,4 +7,23 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: {maximum: 200}, format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
     has_secure_password
+    
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+    
+    # Returns a random token.
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+    
+    
+    def remember
+        #remember token is the uncoded token that is not saved to the DB
+        self.remember_token = User.new_token
+        #we encode the remember token into :remember_digest and store this in the DB
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
