4c57e827452e89c840223fc611a5ac562e643566
Matt Lashinsky <malashinsky@gmail.com>
2016-09-07 03:31:22 +0000

    finished up ch9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,7 @@ module SessionsHelper
     end
     
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
     end
@@ -16,6 +17,18 @@ module SessionsHelper
     
     def logged_in?
         !current_user.nil?
+    end
+    
+    def remember(user)
+       user.remember
+       cookies.permanent.signed[:user_id] = user.id
+       cookies.permanent[:remember_token] = user.remember_token
+    end
+    
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
 end
  
